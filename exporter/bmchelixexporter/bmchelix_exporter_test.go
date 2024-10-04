fd18bfcbba8077aa4eb6774e7d60e559092dcdae
Nassim Boutekedjiret <nassim@sentrysoftware.com>
2024-10-04 19:16:09 +0200

    Issue #nnnn: Create BMC Helix Exporter
    Skeleton code and unit tests

---
+++ exporter/bmchelixexporter/bmchelix_exporter_test.go
@@ -0,0 +1,27 @@
+package bmchelixexporter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/exporter/exportertest"
+)
+
+
+func TestNewBmcHelixExporterWithNilConfig(t *testing.T) {
+	t.Parallel()
+
+	exp, err := newBmcHelixExporter(nil, exportertest.NewNopSettings())
+	assert.Nil(t, exp)
+	assert.Error(t, err)
+}
+
+func TestNewBmcHelixExporterWithDefaultConfig(t *testing.T) {
+	t.Parallel()
+
+	cfg := createDefaultConfig().(*Config)
+	exp, err := newBmcHelixExporter(cfg, exportertest.NewNopSettings())
+	assert.NotNil(t, exp)
+	assert.NoError(t, err)
+}
+
---
+++ exporter/bmchelixexporter/factory_test.go
@@ -0,0 +1,36 @@
+package bmchelixexporter
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/exporter/exportertest"
+)
+
+func TestCreateDefaultConfig(t *testing.T) {
+	cfg := createDefaultConfig()
+	assert.NotNil(t, cfg, "failed to create default config")
+	assert.NoError(t, componenttest.CheckConfigStruct(cfg))
+}
+
+func TestCreateMetricsExporter(t *testing.T) {
+	cfg := createDefaultConfig()
+	_, err := createMetricsExporter(context.Background(), exportertest.NewNopSettings(), cfg)
+	assert.NoError(t, err)
+}
+
+func TestCreateInstanceViaFactory(t *testing.T) {
+	factory := NewFactory()
+
+	cfg := factory.CreateDefaultConfig()
+	exp, err := factory.CreateMetricsExporter(
+		context.Background(),
+		exportertest.NewNopSettings(),
+		cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, exp)
+
+	assert.NoError(t, exp.Shutdown(context.Background()))
+}
